99941c26e5d07aeecf500a831fd48ee085e620b7
prateek0511 <47604296+prateek0511@users.noreply.github.com>
2024-07-09 16:32:13 +0000

    Variable code change

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,9 +1,13 @@
 const accountId=144553
+//let is used for defined vairables and it will be used everywhere
 let accountEmail="malhotraanuj.729@gmail.com"
+//var is used for defining variable but this has a block scope and functional scope issue thats why it is not used now a days
 var accountPassword="12345"
 accountCity="Jaipur"
 
-accountId=2
-
+//accountId=2
+ accountEmail="malhotra.729@gmail.com"
+ accountPassword="123"
 
 console.log(accountId);
+console.table([accountEmail,accountEmail,accountCity])
