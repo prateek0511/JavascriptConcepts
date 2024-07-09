4d65bf155ceee4a3a0382b44ea85f54ff848edc4
prateek0511 <47604296+prateek0511@users.noreply.github.com>
2024-07-09 17:07:21 +0000

    number change

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,23 @@
+"use strict"; //treat all JS code as newer or latest version
+
+// alert("Hello") // we are using node js not browser
+
+console.log(3+3)  //code readability should be high
+
+let name="Prateek"
+let age=24
+let isLoggedIn=false
+
+//number => 2 to power of 53
+//bigint
+//string=> ""
+//boolean => true/false
+//null => standalone value  (it is a representation of empty value)
+//undefined => when value is not defined
+//symbol => unique 
+
+//object
+console.log(typeof "Prateek")
+console.log(typeof age)
+console.log(typeof null) //object
+console.log(typeof undefined) //undefined
