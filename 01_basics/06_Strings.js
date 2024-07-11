2536bd67569e83715e764e36e43326b41d939702
prateek0511 <47604296+prateek0511@users.noreply.github.com>
2024-07-11 16:12:36 +0000

    string 11thjuly

---
+++ 01_basics/06_Strings.js
@@ -0,0 +1,9 @@
+const name="Prateek"
+
+const repoCount=50
+//OUTDATED method
+// console.log(name+repoCount+"value")
+
+
+//string interpolation as is can directly assign value to placeholders
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`)
