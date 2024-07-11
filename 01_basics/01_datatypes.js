d309f573600862df51e892c66324279f726e4c35
prateek0511 <47604296+prateek0511@users.noreply.github.com>
2024-07-11 15:45:09 +0000

    11th JulyStudy

--- 01_basics/01_datatypes.js
+++ 01_basics/01_datatypes.js
@@ -7,17 +7,32 @@ console.log(3+3)  //code readability should be high
 let name="Prateek"
 let age=24
 let isLoggedIn=false
+//Primitive datatypes
+//1. number => 2 to power of 53
+//2. bigint
+//3. string=> ""
+//4. boolean => true/false
+//5. null => standalone value  (it is a representation of empty value)
+//6. undefined => when value is not defined
+//7.  symbol => unique 
 
-//number => 2 to power of 53
-//bigint
-//string=> ""
-//boolean => true/false
-//null => standalone value  (it is a representation of empty value)
-//undefined => when value is not defined
-//symbol => unique 
-
+// Non Primitive Datatype or Reference Type
 //object
 console.log(typeof "Prateek")
 console.log(typeof age)
 console.log(typeof null) //object
 console.log(typeof undefined) //undefined
+
+//Arrays
+const arr=["value1", "value2"]
+//Object
+let myObj={
+    name:"Prateek",
+    age:24
+}
+//Functions
+
+const myfunch=function(){
+    console.log("Hello world")
+}
+
---
+++ 01_basics/04_Operations.js
@@ -0,0 +1,19 @@
+let value=3
+let negval=-value
+
+console.log(negval)
+
+// +,-,*,/,%
+
+let str1="Hello"
+let str2=" Prateek"
+
+console.log(str1+str2);
+
+console.log("1"+2)
+console.log(1+"2")
+
+let num1 , num2 , num3
+
+num1=num2=num3=2+2
+
